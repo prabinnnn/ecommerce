ecfb28b8ee5335504aa38292cfe294665d9889f6
prabinnnn <prabinbhandari120@gmail.com>
2024-02-13 21:17:28 +0545

    add index

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,4 @@
+const { generateToken } = require("../config/jwtToken");
 const User = require("../model/userModel");
 const asynHandler = require("express-async-handler");
 const createUser = asynHandler(async (req, res) => {
@@ -18,7 +19,14 @@ const loginUserControl = asynHandler(async (req, res) => {
   // console.log(email, password);
   const findUser = await User.findOne({ email });
   if (findUser && (await findUser.isPasswordmatch(password))) {
-    res.json(findUser);
+    res.json({
+      _id: findUser?._id,
+      firstname: findUser?.firstname,
+      lastname: findUser?.lastname,
+      email: findUser?.email,
+      mobile: findUser?.mobile,
+      token: generateToken(findUser?.id),
+    });
   } else {
     console.log("invalid credent");
   }
