651a9a7403ffa26f3e50923a4d97d129d41bb8df
prabinnnn <prabinbhandari120@gmail.com>
2024-02-17 19:01:56 +0545

    add index

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const {
   getAllUser,
   getaUser,
   deleteaUser,
+  updateUser,
 } = require("../controller/userController");
 const router = require("express").Router();
 router.post("/register", createUser);
@@ -11,4 +12,5 @@ router.post("/login", loginUserControl);
 router.get("/alluser", getAllUser);
 router.get("/:id", getaUser);
 router.delete("/:id", deleteaUser);
+router.put("/:id", updateUser);
 module.exports = router;
