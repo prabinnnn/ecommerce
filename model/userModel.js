e420d8f46b7b413178f17da652496ccea413de4f
prabinnnn <prabinbhandari120@gmail.com>
2024-03-23 20:23:58 +0545

    add index

--- model/userModel.js
+++ model/userModel.js
@@ -1,41 +1,46 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const userSchema = new mongoose.Schema({
-  firstname: {
-    type: String,
-    required: true,
-    index: true,
+const userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    roles: {
+      type: String,
+      default: "user",
+    },
+    cart: {
+      type: Array,
+      default: [],
+    },
+    address: [{ type: ObjectId, ref: "Address" }],
+    wishlist: [{ type: ObjectId, ref: "Product" }],
   },
-  lastname: {
-    type: String,
-    required: true,
-    index: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  mobile: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  roles: {
-    type: String,
-    default: "user",
-  },
-  cart: {
-    type: Array,
-    default: [],
-  },
-  address: [{ type: ObjectId, ref: "Address" }],
-  wishlist: [{ type: ObjectId, ref: "Product" }],
-});
+  {
+    timestamps: true,
+  }
+);
 userSchema.pre("save", async function (next) {
   const salt = bcrypt.genSaltSync(10);
   this.password = await bcrypt.hash(this.password, salt);
