6495cab4884f98af2c6eefe07d2797a846ce4d3a
prabinnnn <prabinbhandari120@gmail.com>
2024-03-24 20:50:14 +0545

    add index

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -4,6 +4,9 @@ const asyncHandler = require("express-async-handler");
 const authMiddler = asyncHandler((req, res, next) => {
   let token;
   if (req?.headers?.authorization?.startsWith("bearer")) {
+    token = req.header.authorization.split(" ")[1];
+    try {
+    } catch (e) {}
   } else {
     throw new Error("there is no token attached to it");
   }
