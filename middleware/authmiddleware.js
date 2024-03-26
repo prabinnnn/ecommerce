97b3e81124a7c4d884dc86bd01fd18b4b58528cc
prabinnnn <prabinbhandari120@gmail.com>
2024-03-26 21:10:20 +0545

    add index

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -1,14 +1,16 @@
 const user = require("../model/userModel");
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
-const authMiddler = asyncHandler((req, res, next) => {
+const authMiddler = asyncHandler(async (req, res, next) => {
   let token;
   if (req?.headers?.authorization?.startsWith("bearer")) {
     token = req.header.authorization.split(" ")[1];
     try {
       if (token) {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log(decoded);
+        const user = await User.findById(decoded?.id);
+        req.user = user;
+        next();
       }
     } catch (e) {
       throw new Error("no authorization token is expired. plz login ");
