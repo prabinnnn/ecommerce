0576fe0fdb1e1b16c5f2d23ec1b78ec1e9e2bc0b
prabinnnn <prabinbhandari120@gmail.com>
2024-04-04 19:49:46 +0545

    add index

--- utils/validateMongodb.js
+++ utils/validateMongodb.js
@@ -1,4 +1,5 @@
 const mongooes = require("mongoose");
 const validateMongooseId = (Id) => {
   const isValid = mongooes.Schema.Types.ObjectId.isValid(Id);
+  if (!isValid) throw new Error("this is not validate");
 };
